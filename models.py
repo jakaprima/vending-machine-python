c8651aa2a1ff2bc1032dc0925b019bee7118b924
jaka.maulana <jaka@motionpay.id>
2023-12-14 15:37:01 +0700

    fix: del unused code

--- models.py
+++ models.py
@@ -1,11 +1,7 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/db_vending_machine"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
